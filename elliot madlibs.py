2d7d05337ecc7cd6fb04ab39332b3db7a7327b1b
Elliot Kim <penguin2485coder@penguincoding.org>
2024-07-29 14:50:18 -0400

    Add files via upload

---
+++ elliot madlibs.py
@@ -0,0 +1,4 @@
+print("hello")
+noun = input ("enter a noun")
+verb = input ("enter a verb ending in ed")
+print("i went to the pool there were so meny " + noun + " that i " + verb + " into the pool")
---
+++ quiz.py
@@ -0,0 +1,10 @@
+Python 3.12.4 (tags/v3.12.4:8e8a4ba, Jun  6 2024, 19:30:16) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\stude\AppData\Local\Programs\Python\Python312\if else elif.py
+true or false 100,000,0000,000 is a numberg
+INCORECT the nomber had 4 diggits without a collin
+what is the most popular water park in the worldg
+INCORECT the Chimelong Water Park is the moast popular
+what is the biggest carnivorus dinosaurg
+INCORECT the biggest is the spinosaurus
